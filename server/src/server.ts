6d420cd8d6992c952247d4228ced242a02ecbbd0
Agustinho Neto <agustinho.pneto@gmail.com>
2020-06-02 20:27:24 -0300

    :sparkles: Finalizado em upload!

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,12 @@
 /* eslint-disable no-console */
 import express from 'express';
+import cors from 'cors';
 import path from 'path';
 import routes from './routes';
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(routes);
